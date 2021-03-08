510158f7ce6f9b45f95c92ed78d35e94708c6438
olesya <olesya.vegrenovskaya@gmail.com>
2021-03-08 04:08:51 +0100

    Zasedačky (vypsat, kolik je volných místností)

--- priklad03.py
+++ priklad03.py
@@ -7,6 +7,7 @@ volnePokoje = {
 a = int(input('Zadejte čas meetingů:'))
 if a in volnePokoje:
     print(volnePokoje[a])
+    print(f"Celkem volné {len(volnePokoje[a])} pokoje.")
 else:
     print('V ten čas nejsou k dispozici volné meetingové místnosti')
 
