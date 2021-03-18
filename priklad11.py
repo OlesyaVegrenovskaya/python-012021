b0447443dcc7d05f92a04a8b4bbf58f52aa8d345
olesya <olesya.vegrenovskaya@gmail.com>
2021-03-18 02:06:22 +0100

    Evidence aut

--- priklad11.py
+++ priklad11.py
@@ -5,17 +5,5 @@ class Auto:
         self.mileage = mileage
         self.free_auto = True
 
-    def free_auto(self):
-        self.free_auto = False
-
-    def getInfo(self):
-        if self.free_auto:
-            return f"Auto {self.car_type}  která má registrační značku {self.car_plate} a {self.mileage} počet najetých kilometrů je k dispozici"
-        return f"Auto {self.car_type} která má registrační značku {self.car_plate} a {self.mileage} počet najetých kilometrů neni k dispozici"
-
-
 Peugeot = Auto("4A2 3020", "Peugeot 403 Cabrio", 47534)
 Skoda = Auto("1P3 4747", "Škoda Octavia", 41253)
-
-print(Peugeot.getInfo())
-print(Skoda.getInfo())
