90fc5f2b0d291f3001f8e00b4fb40a1681d17e8e
olesya <olesya.vegrenovskaya@gmail.com>
2021-04-23 01:26:40 +0200

    Založení projektu

--- pujcovna/katalog/views.py
+++ pujcovna/katalog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views import View
 # Create your views here.
+
+class IndexView(View):
+    def get(self, request):
+        return HttpResponse("Zde bude titulní stránka.")
+
+
+class SeznamView(View):
+    def get(self, request):
+        return HttpResponse("Zde bude seznam aut.")
+
