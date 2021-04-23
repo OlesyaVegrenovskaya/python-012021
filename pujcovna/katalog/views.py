84ebd0d0036883a768df577de9f208ba1ab25f61
olesya <olesya.vegrenovskaya@gmail.com>
2021-04-23 02:16:15 +0200

    Založení projektu

--- pujcovna/katalog/views.py
+++ pujcovna/katalog/views.py
@@ -10,5 +10,8 @@ class IndexView(View):
 
 class SeznamView(View):
     def get(self, request):
-        return HttpResponse("Zde bude seznam aut.")
+        return HttpResponse("<h1>Vítejte v naší autopůjčovně!</h1>"
+                            "<a href='http://localhost:8000/katalog/seznam/'>Jaká auta máme?</a> <br>"
+                            "<h2>O naší autopůjčovně</h2>"
+                            "<p>Naše půjčovna vznikla v roce 2011 a dnes nabízí přibližně 30 aut.</p>")
 
