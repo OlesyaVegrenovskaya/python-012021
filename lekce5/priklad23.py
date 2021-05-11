9ec86d60ff27687faa7794db52ecb21ae82ebc17
olesya <olesya.vegrenovskaya@gmail.com>
2021-05-11 19:00:07 +0200

    Twilio

--- lekce5/priklad23.py
+++ lekce5/priklad23.py
@@ -2,3 +2,15 @@ import pandas
 import wget
 wget.download("https://raw.githubusercontent.com/pesikj/python-012021/master/zadani/5/country_vaccinations.csv")
 country = pandas.read_csv("country_vaccinations.csv")
+
+total_vaccinated = country.loc[(country["date"] == "2021-03-10"), ["country", "total_vaccinations"]]
+print(total_vaccinated)
+
+million = country.loc[(country["date"] == "2021-03-10") & (country["total_vaccinations"] > 1_000_000), ["country", "total_vaccinations"]]
+print(million)
+
+more_than_thousand = country.loc[(country["date"] == "2021-03-10") & (country["daily_vaccinations"] > 100_000)]
+print(more_than_thousand)
+
+less_than_thousand = country.loc[(country["date"]== "2021-03-10") & (country["daily_vaccinations"] < 100_000)]
+print(less_than_thousand)
