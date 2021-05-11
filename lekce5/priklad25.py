e05a0c45efab060cf6ac9c712f0afc2d726ac1e4
olesya <olesya.vegrenovskaya@gmail.com>
2021-05-11 18:59:56 +0200

    Twilio

--- lekce5/priklad25.py
+++ lekce5/priklad25.py
@@ -1,2 +1,14 @@
+import pandas
 import wget
-wget.download("https://raw.githubusercontent.com/pesikj/python-012021/master/zadani/5/twlo.csv")
+
+wget.download("https://raw.githubusercontent.com/pesikj/python-012021/master/zadani/5/temperature.csv")
+temperature = pandas.read_csv('temperature.csv')
+
+print(temperature.head())
+
+print(temperature.loc[temperature["Day"] == 13])
+
+table = temperature.loc[(temperature["Day"] == 13) & (temperature["Country"] == "US")]
+print(table)
+
+print(table[(table["City"] == "Washington") | (table["City"] == "Philadelphia")])
