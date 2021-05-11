3f9d3e789d79026cfc81de4f9d0b8c3e377fff50
olesya <olesya.vegrenovskaya@gmail.com>
2021-05-11 18:59:44 +0200

    Twilio

--- lekce5/priklad21.py
+++ lekce5/priklad21.py
@@ -1,9 +1,12 @@
 import pandas
 import wget
-#wget.download("https://raw.githubusercontent.com/pesikj/python-012021/master/zadani/5/twlo.csv")
+wget.download("https://raw.githubusercontent.com/pesikj/python-012021/master/zadani/5/twlo.csv")
 twlo=pandas.read_csv("twlo.csv")
+
 print(twlo.shape)
 print(twlo.iloc[:5])
 print(twlo.head())
 print(twlo.iloc[-1])
 print(twlo.tail())
+
+
